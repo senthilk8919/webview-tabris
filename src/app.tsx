7c90bd894e20a7122e75f164b73b15c691d97fc5
senthilk8919 <senthilk8919@gmail.com>
2019-11-12 20:21:45 +0400

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ let text = "kosong"
 
 ui.contentView.append(
     <composite left={0} right={0} top={0} bottom={0}>
-        <webView  left={0} right={0} top={0} bottom={0} url='http://fe-omah.chickenkiller.com/'/>
+        <webView  left={0} right={0} top={0} bottom={0} url='http://www.skynetsoftware.ga/'/>
     </composite>
 );
 
