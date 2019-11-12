eee1349265713956a06d5c3e837ffa06c461ae78
senthilk8919 <senthilk8919@gmail.com>
2019-11-12 21:14:51 +0400

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import { Button, Composite, TextView, WebView, ui } from 'tabris';
 
-
+let text = "Skynet"
 
 ui.contentView.append(
-    <composite>
+    <composite left={0} right={0} top={0} bottom={0}>
         <webView  left={0} right={0} top={0} bottom={0} url='http://www.skynetsoftware.ga/'/>
     </composite>
 );
